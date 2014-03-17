c1bfa260dbf36c3d6f93a1ded57030d2e53fa053
Jordan Thomas <mail@jordanthomas.me>
2014-03-16 20:38:28 -0400

    Adds jade sass, some base styles.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 var gulp   = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var jade   = require('gulp-jade');
+var sass   = require('gulp-sass')
 
 var paths = {
-  html: ['*.html'],
   scripts: ['js/**/*.js', '!js/vendor/**/*.js'],
+  pages: ['**/*.jade', '!layouts/**', '!node_modules/**'],
+  layouts: ['layouts/**/*.jade'],
+  scss: ['css/**/*.scss', '!css/vendor/*.scss']
 };
 
 gulp.task('scripts', function() {
@@ -14,13 +18,22 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest('build/js'));
 });
 
-gulp.task('copy', function(){
-  return gulp.src(paths.html)
-    .pipe(gulp.dest('build'));
+gulp.task('scss', function () {
+  return gulp.src(paths.scss)
+    .pipe(sass())
+    .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('pages', function() {
+  return gulp.src(paths.pages)
+    .pipe(jade({ pretty: true }))
+    .pipe(gulp.dest('build/'))
 });
 
 gulp.task('watch', function() {
   gulp.watch(paths.scripts, ['scripts']);
+  gulp.watch(paths.pages, ['pages']);
+  gulp.watch(paths.layouts, ['pages']);
 });
 
-gulp.task('default', ['scripts', 'copy', 'watch']);
+gulp.task('default', ['scripts', 'scss', 'pages', 'watch']);
