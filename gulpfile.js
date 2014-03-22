f38000335b82b645c56daccbcf1fb380b35ee23f
Jordan Thomas <jthomas@barefootproximity.com>
2014-03-22 14:43:02 -0400

    Flesh out Gulp tasks and project structure.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
-var gulp   = require('gulp');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var jade   = require('gulp-jade');
-var sass   = require('gulp-sass')
-
-var paths = {
-  scripts: ['js/**/*.js', '!js/vendor/**/*.js'],
-  pages: ['**/*.jade', '!layouts/**', '!node_modules/**'],
-  layouts: ['layouts/**/*.jade'],
-  scss: ['css/**/*.scss', '!css/vendor/*.scss']
-};
+var gulp     = require('gulp'),
+    http     = require('http'),
+    ecstatic = require('ecstatic'),
+    concat   = require('gulp-concat'),
+    uglify   = require('gulp-uglify'),
+    jade     = require('gulp-jade'),
+    sass     = require('gulp-sass'),
+    paths = {
+      scripts: ['js/**/*.js', '!js/vendor/**/*.js'],
+      pages: ['**/*.jade', '!layouts/**', '!node_modules/**'],
+      layouts: ['layouts/**/*.jade'],
+      scss: ['css/**/*.scss', '!css/vendor/**/*.scss'],
+    };
 
 gulp.task('scripts', function() {
   return gulp.src(paths.scripts)
@@ -19,21 +20,30 @@ gulp.task('scripts', function() {
 });
 
 gulp.task('scss', function () {
-  return gulp.src(paths.scss)
-    .pipe(sass())
+  return gulp.src('css/main.scss')
+    .pipe(sass({errLogToConsole: true}))
     .pipe(gulp.dest('build/css'));
 });
 
 gulp.task('pages', function() {
   return gulp.src(paths.pages)
     .pipe(jade({ pretty: true }))
-    .pipe(gulp.dest('build/'))
+    .pipe(gulp.dest('build'))
+});
+
+gulp.task('server', function() {
+  http.createServer(
+    ecstatic({ root: __dirname + '/build' })
+  ).listen(8080);
+
+  console.log('Server listening on 8080...');
 });
 
 gulp.task('watch', function() {
   gulp.watch(paths.scripts, ['scripts']);
+  gulp.watch(paths.scss, ['scss']);
   gulp.watch(paths.pages, ['pages']);
   gulp.watch(paths.layouts, ['pages']);
 });
 
-gulp.task('default', ['scripts', 'scss', 'pages', 'watch']);
+gulp.task('default', ['scripts', 'scss', 'pages', 'server', 'watch']);
